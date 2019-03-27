72fced857e79f3bede9d43a97fda6fecd8d00b4e
Nirmal Nepal <rockink_nirmal@live.com>
2019-03-26 22:44:04 -0400

    Designing component phase 2

--- src/product/components/Product.tsx
+++ src/product/components/Product.tsx
@@ -4,19 +4,28 @@ import { Product } from '../Domains';
 
 import './product.css';
 
-export interface ProductComponentProp{product: Product, key?: any};
-export const ProductComponent = (productComponentProp: ProductComponentProp)=>(
-    <div className="product">
-        <h1>{productComponentProp.product.name}</h1>
-        <p>
-            {productComponentProp.product.cpu} | 
-            {productComponentProp.product.ram} | 
-            {productComponentProp.product.opSys}
-        </p>
+
+
+export interface ProductComponentProp { product: Product, key?: any };
+export const ProductComponent = (productComponentProp: ProductComponentProp) => (
+    <div className="product-wrapper">
+        <div className="product">
+            <div className="left-component">
+                    <p style={{background: 'green', height:'100%', margin: '0px'}}></p>
+            </div>
+            <div className="right-component">
+                <h1>{productComponentProp.product.name}</h1>
+                <p>
+                    {productComponentProp.product.cpu} |
+                {productComponentProp.product.ram} |
+                {productComponentProp.product.opSys}
+                </p>
+            </div>
+        </div>
     </div>
 )
 
-export interface ProductsProps{products: Product[]}
+export interface ProductsProps { products: Product[] }
 export const ProductsComponent = (productProps: ProductsProps) => (
     <div className="ProductsComponent">
         {
