1a792fd6d4479973794d0f5bd0fce7a8efb89fce
Nirmal Nepal <rockink_nirmal@live.com>
2019-03-26 22:56:19 -0400

    Design: added materail design. Card encapsulates product

--- src/product/components/Product.tsx
+++ src/product/components/Product.tsx
@@ -1,28 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Product } from '../Domains';
-
+import Card from '@material-ui/core/Card';
 import './product.css';
 
 
 
 export interface ProductComponentProp { product: Product, key?: any };
 export const ProductComponent = (productComponentProp: ProductComponentProp) => (
-    <div className="product-wrapper">
+    <Card className="product-wrapper">
         <div className="product">
             <div className="left-component">
-                    <p style={{background: 'green', height:'100%', margin: '0px'}}></p>
+                    <p style={{background: 'rgb(232, 233, 232)', height:'100%', margin: '0px'}}></p>
             </div>
             <div className="right-component">
                 <h1>{productComponentProp.product.name}</h1>
                 <p>
                     {productComponentProp.product.cpu} |
-                {productComponentProp.product.ram} |
-                {productComponentProp.product.opSys}
+                    {productComponentProp.product.ram} |
+                    {productComponentProp.product.opSys}
                 </p>
             </div>
         </div>
-    </div>
+    </Card>
 )
 
 export interface ProductsProps { products: Product[] }
