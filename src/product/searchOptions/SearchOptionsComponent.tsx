1d55c2587e9774008ccbf51ca9769a99cfca9869
Nirmal Nepal <rockink_nirmal@live.com>
2019-04-08 22:07:17 -0400

    Response Product Filters: Collapsing view design
    It is not ideal to show the list of the categories for the user to select in the mobile, as the device screen is small. Therefore, one way to tackle this is to build a collapsing view. Given a Filter Brands, we design an option to open/close the section.
    
    It uses React hooks to consider the state. Also, material icons for add and substract are used as well to indicate wheher the filter list is currently collapsed or open.

--- src/product/searchOptions/SearchOptionsComponent.tsx
+++ src/product/searchOptions/SearchOptionsComponent.tsx
@@ -1,16 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import searchComponentService, { Filter } from './SearchComponentService';
 
+import './SearchOptionComponent.css';
+import { Icon } from '@material-ui/core';
 
 interface SearchFilterProps { filter: Filter }
+interface SearchFilterState{isOpen: false}
+
 const SearchFilterComponent = (props: SearchFilterProps) => {
     const { filter } = props;
+    const [state, setToggleOn] = useState({isOpen: false});
+    const [selectedState, setSelected] = useState<boolean[]>([]);
+    const icon = state.isOpen ? "minimize": "add";
     return (
         <div className="SearchFilter">
-            <h4>{filter.name}</h4>
-            {
-                filter.filterOptions.map((brand, i) => <li key={i}>{brand.key} ({brand.count})</li>)
-            }
+            <div className="header">
+                <p>{filter.name}</p>
+                <p onClick={e=>setToggleOn({isOpen: !state.isOpen})} className="header-icon"><Icon>{icon}</Icon></p>
+            </div>
+            <div className={`filterOptions ${state.isOpen?"filter-open":"filter-close"}`}>
+                <ul className="filter-list">
+                    {filter.filterOptions.map((brand, i) => (
+                        <li key={i} onClick={e=>{
+                            setSelected(state=>{
+                                state[i] = state[i] === undefined ? true : !state[i]
+                                return [...state];
+                            })}} >
+
+                            <input defaultChecked={selectedState[i]} type="checkbox"  /> {brand.key} ({brand.count})</li>
+                    ))}
+                </ul>
+            </div>
         </div>
     );
 }
